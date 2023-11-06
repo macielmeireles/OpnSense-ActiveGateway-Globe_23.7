72033e6d4d6fd3d89bf6d41d8fef30cefaa7a1f4
Maciel Meireles <132109923+macielmeireles@users.noreply.github.com>
2023-11-06 19:23:56 -0300

    Install v0.4: Now with OPNsense Version Check
    **Version Check:**
    - Added `Version Check:` Implemented a version check for OPNsense using a variable and command, with an error message displayed for mismatched versions.
    
    **New Options:**
    
    - Added `--help` option to the script. If provided, the script displays usage instructions.
    - Added `--restore` option to the script. If provided, the script downloads the original version of the `gateways.widget.php` file from GitHub.
    - Added `--check` option to the script. If provided, the script checks the OPNsense version and informs if it's the correct version or if it needs to be updated.
    
    **User Feedback:**
    - Added messages after installation and restoration to inform the user that the script was executed successfully or that the restoration was completed.

---
+++ install.sh
@@ -0,0 +1,71 @@
+#!/bin/sh
+
+# OpnSense Active Gateway Globe Script
+# Version: 0.4 (beta)
+# Date: 01/11/2023
+# Author: Maciel Meireles
+# GitHub: https://github.com/macielmeireles/OpnSense-ActiveGateway-Globe
+
+# Define the correct version of OPNsense
+correct_version="OPNsense 23.7.7_3"
+
+# Define the names of the files to be downloaded
+gateway_file="get_default_gateway_v.02.php"
+widget_file="script_change_widget_v0.5.php"
+
+# Define the base URL
+base_url="https://raw.githubusercontent.com/macielmeireles/OpnSense-ActiveGateway-Globe/main/"
+
+# Define the full URLs of the files to be downloaded
+gateway_url="${base_url}${gateway_file}"
+widget_url="${base_url}${widget_file}"
+
+# Define the URL of the original file
+original_url="https://raw.githubusercontent.com/opnsense/core/master/src/www/widgets/widgets/gateways.widget.php"
+
+# Get the current version of OPNsense
+current_version=$(opnsense-version)
+
+# Check the provided option
+case "$1" in
+    --help)
+        echo "- To install, use: ./install"
+        echo "- To restore, use: ./install --restore"
+        echo "- To check the version, use: ./install --check"
+        ;;
+    --restore)
+        # Download the original file
+        fetch -o gateways.widget.php $original_url
+        echo ""
+        echo ""
+        echo "Restore Complete from Github!"
+        ;;
+    --check)
+        # Check if the current version is the correct version
+        if [ "$current_version" != "$correct_version" ]; then
+            echo "The current version of OPNsense ($current_version) is not the correct version ($correct_version). Please update OPNsense."
+        else
+            echo "The current version of OPNsense ($current_version) is the correct version."
+        fi
+        ;;
+    *)
+        # Check if the current version is the correct version
+        if [ "$current_version" != "$correct_version" ]; then
+            echo "The current version of OPNsense ($current_version) is not the correct version ($correct_version). Please update OPNsense before running this script."
+            exit 1
+        fi
+
+        # Change to the desired directory
+        cd /usr/local/www/widgets/widgets/
+
+        # Download the files and rename them
+        fetch -o get_default_gateway.php $gateway_url
+        fetch -o script_change_widget.php $widget_url
+
+        # Execute the PHP script
+        php script_change_widget.php
+        echo ""
+        echo ""
+        echo "Script executed successfully!"
+        ;;
+esac
