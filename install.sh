aa09bfda613916d73241ae5a8f9c6e182f1b34aa
Maciel Meireles <132109923+macielmeireles@users.noreply.github.com>
2023-11-23 10:34:27 -0300

    New Inst
    * Updated the script version to 0.5. Changes:
      1. Replaced the ‘correct_version’ variable with an array ‘tested_versions’ to allow for compatibility with multiple versions of OPNsense.
      2. Modified the script to check if the current version of OPNsense is in the ‘tested_versions’ array before proceeding.
    
    * Upcoming Feature: Enhanced Version Compatibility Check
      - In the next version of the installer, a comparator will be added to the original OpnSense code. If the widget page remains unchanged, the script will indicate that it's safe to update the code and will perform the update securely, regardless of the version.

---
+++ install.sh
@@ -0,0 +1,76 @@
+Sim, você está correto. "Tested versions" pode ser uma escolha melhor para o nome da variável, pois indica que as versões listadas foram testadas e são compatíveis com o script. Aqui está o código atualizado:
+
+```bash
+#!/bin/sh
+
+# OpnSense Active Gateway Globe Script
+# Version: 0.5 (beta)
+# Date: 11/23/2023
+# Author: Maciel Meireles
+# GitHub: https://github.com/macielmeireles/OpnSense-ActiveGateway-Globe
+
+# Define the tested versions of OPNsense
+tested_versions=("OPNsense 23.7.7_3" "OPNsense 23.7.8_1")
+
+# Define the names of the files to be downloaded
+gateway_file="get_default_gateway_v.02.php"
+widget_file="script_change_widget_v0.5.php"
+
+# Define the base URL
+base_url="https://raw.githubusercontent.com/macielmeireles/OpnSense-ActiveGateway-Globe/main/"
+
+# Define the full URLs of the files to be downloaded
+gateway_url="${base_url}${gateway_file}"
+widget_url="${base_url}${widget_file}"
+
+# Define the URL of the original file
+original_url="https://raw.githubusercontent.com/opnsense/core/master/src/www/widgets/widgets/gateways.widget.php"
+
+# Get the current version of OPNsense
+current_version=$(opnsense-version)
+
+# Check the provided option
+case "$1" in
+    --help)
+        echo "- To install, use: ./install"
+        echo "- To restore, use: ./install --restore"
+        echo "- To check the version, use: ./install --check"
+        ;;
+    --restore)
+        # Download the original file
+        fetch -o gateways.widget.php $original_url
+        echo ""
+        echo ""
+        echo "Restore Complete from Github!"
+        ;;
+    --check)
+        # Check if the current version is in the tested versions
+        if [[ ! " ${tested_versions[@]} " =~ " ${current_version} " ]]; then
+            echo "The current version of OPNsense ($current_version) is not in the tested versions (${tested_versions[*]}). Please update OPNsense."
+        else
+            echo "The current version of OPNsense ($current_version) is in the tested versions."
+        fi
+        ;;
+    *)
+        # Check if the current version is in the tested versions
+        if [[ ! " ${tested_versions[@]} " =~ " ${current_version} " ]]; then
+            echo "The current version of OPNsense ($current_version) is not in the tested versions (${tested_versions[*]}). Please update OPNsense before running this script."
+            exit 1
+        fi
+
+        # Change to the desired directory
+        cd /usr/local/www/widgets/widgets/
+
+        # Download the files and rename them
+        fetch -o get_default_gateway.php $gateway_url
+        fetch -o script_change_widget.php $widget_url
+
+        # Execute the PHP script
+        php script_change_widget.php
+        echo ""
+        echo ""
+        echo "Script executed successfully!"
+        ;;
+esac
+```
+Espero que isso ajude! Se você tiver mais perguntas, fique à vontade para perguntar.
